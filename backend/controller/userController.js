de8f97763a87edd31b2e6daf03e9cb03977abd2e
priyanshsingh <priyansh@singh.com>
2022-07-27 14:43:56 +0530

    building error middleware

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,10 +3,17 @@ const asyncHandler = require("express-async-handler");
 // @desc    Register a new user
 // @route   /api/users
 // @access  Public
-const registerUser = (req, res) => {
-  console.log(req.body);
-  res.send("Register User");
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+
+  res.send("Register Route");
+});
 
 // @desc    Login a user
 // @route   /api/users/login
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -8,11 +9,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  // res.send("Hello");
-  res.status(200).json({ message: "Welcome to the group" });
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
 });
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
