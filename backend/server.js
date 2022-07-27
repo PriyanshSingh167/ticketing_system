83eae68d4fed6b3b57399a641ae7601189e8601a
priyanshsingh <priyansh@singh.com>
2022-07-27 13:58:08 +0530

    Sending data through postman and setting up json(body-parser middleware

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  // res.send("Hello");
+  res.status(200).json({ message: "Welcome to the group" });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
