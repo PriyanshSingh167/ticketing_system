ef8276dddbc6d1001a755d4baed92fd7b91422d3
priyanshsingh <priyansh@singh.com>
2022-07-29 12:21:51 +0530

    protect routes and authenciation

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,21 +10,24 @@ const protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
-      // get token from header => token is in form "Bearer token" hence we will split token
+      // Get token from header
       token = req.headers.authorization.split(" ")[1];
-      // verify token
+      // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      // get user from token and remove password
+      // Get user from token
       req.user = await User.findById(decoded.id).select("-password");
+
+      next();
     } catch (error) {
       console.log(error);
       res.status(401);
-      throw new Error("Not Authorized");
+      throw new Error("Not authorized");
     }
   }
+
   if (!token) {
     res.status(401);
-    throw new Error("Not Authorized");
+    throw new Error("Not authorized");
   }
 });
 
